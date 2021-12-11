6c74a2163d5a044d26c3ee8f85c55f05ea757147
roja71 <90477838+roja71@users.noreply.github.com>
2021-12-11 21:47:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Registerform  from "./components/Forms/register_form"
+import Signinform from './components/Forms/Signin';
+//import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    //<div className="App">
+     // <header className="App-header">
+       // <img src={logo} className="App-logo" alt="logo" />
+        //<p>
+       //   Edit <code>src/App.js</code> and save to reload.
+       // </p>
+       // <a
+         // className="App-link"
+         // href="https://reactjs.org"
+         // target="_blank"
+         // rel="noopener noreferrer"
+       // >
+        //  Learn React
+       // </a>
+      //</header>
+    //</div>
+    //<div>
+     // <Navbar/>
+   // </div>
+   <div>
+     <Signinform />
+    </div>
+  );
+}
+
+export default App;
